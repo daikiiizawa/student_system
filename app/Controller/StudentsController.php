ad05808c4a897fe64ac3a0fc3ad3fb648ddac54e
daikiiizawa <daiki.813.gon@gmail.com>
2016-11-10 02:38:03 +0000

    students/editの作成

--- app/Controller/StudentsController.php
+++ app/Controller/StudentsController.php
@@ -56,8 +56,45 @@ class StudentsController extends AppController{
 
     }
 
-    public function edit(){
+    public function edit ($id = null) {
+        if (!$this->Student->exists($id)) {
+            throw new NotFoundException('生徒情報が見つかりません');
+        }
 
+        $sex = ['男性', '女性', '不明'];
+        $this->set('sex', $sex);
+        $purpose = ['起業', '転職', 'フリーランス', 'スキルアップ', '副業', '趣味', 'その他'];
+        $this->set('purpose', $purpose);
+        $pc = ['Mac', 'Windows7', 'Windows8', 'Windows10', 'その他'];
+        $this->set('pc', $pc);
+        $week = ['日', '月', '火', '水', '木', '金', '土'];
+        $this->set('week', $week);
+        $status = ['入会前', '学習中', '卒業済', '削除予定'];
+        $this->set('status', $status);
+        $yomi = ['A', 'B', 'C'];
+        $this->set('yomi', $yomi);
+        $month = [
+            '01月' => '01月',
+            '02月' => '02月',
+            '03月' => '03月',
+            '04月' => '04月',
+            '05月' => '05月',
+            '06月' => '06月',
+            '07月' => '07月',
+            '08月' => '08月',
+            '09月' => '09月',
+            '10月' => '10月',
+            '11月' => '11月',
+            '12月' => '12月'
+            ];
+        $this->set('month', $month);
+
+        // 詳細ページからgetで来た場合のみdataに既存の生徒情報を参照する
+        if (!$this->request->data) {
+            $this->request->data = $this->Student->findById($id);
+        }
+        $id = $this->request->data['Student']['id'];
+        $this->set('id', $id);
     }
 
     public function view($id = null){
