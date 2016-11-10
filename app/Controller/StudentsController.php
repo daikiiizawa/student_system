397aad47d6fe957a6d3a0cb6d122ddad18a77ba2
daikiiizawa <daiki.813.gon@gmail.com>
2016-11-10 02:34:26 +0000

    students/viewの作成

--- app/Controller/StudentsController.php
+++ app/Controller/StudentsController.php
@@ -60,8 +60,26 @@ class StudentsController extends AppController{
 
     }
 
-    public function view(){
+    public function view($id = null){
+        if (!$this->Student->exists($id)) {
+            throw new NotFoundException('生徒情報がみつかりません');
+        }
+        $student = $this->Student->findById($id);
+        $this->set('student', $student);
 
+        // 指定項目の入力情報
+        $sex = ['男性', '女性', '不明'];
+        $this->set('sex', $sex);
+        $purpose = ['起業', '転職', 'フリーランス', 'スキルアップ', '副業', '趣味', 'その他'];
+        $this->set('purpose', $purpose);
+        $pc = ['Mac', 'Windows7', 'Windows8', 'Windows10', 'その他'];
+        $this->set('pc', $pc);
+        $week = ['日', '月', '火', '水', '木', '金', '土'];
+        $this->set('week', $week);
+        $status = ['入会前', '学習中', '卒業済', '削除予定'];
+        $this->set('status', $status);
+        $yomi = ['A', 'B', 'C'];
+        $this->set('yomi', $yomi);
     }
 
     public function entry(){
